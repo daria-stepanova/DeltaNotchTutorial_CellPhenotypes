9f8329df2cc1567b844294d0b36a6fc1aa408b31
daria-stepanova <daria.stepanova.zavalei@gmail.com>
2022-12-01 10:22:47 +0100

    cpp files2

---
+++ apps/src/Exe_DeltaNotchTutorial.cpp
@@ -0,0 +1,81 @@
+/*
+
+Copyright (c) 2005-2016, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+#include <cxxtest/TestSuite.h>
+
+#include "AbstractCellBasedTestSuite.hpp"
+
+#include "DeltaNotchTutorialSimulation.hpp"
+
+#include "Debug.hpp"
+#include "LogFile.hpp"
+#include "ExecutableSupport.hpp"
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/lexical_cast.hpp>
+
+/*
+ * Prototype functions
+ */
+int main(int argc, char *argv[])
+{
+    ExecutableSupport::StandardStartup(&argc, &argv);
+
+    int exit_code = ExecutableSupport::EXIT_OK;
+
+    // You should put all the main code within a try-catch, to ensure that
+    // you clean up PETSc before quitting.
+    try
+    {
+        DeltaNotchTutorialSimulation sim = DeltaNotchTutorialSimulation();
+        sim.VertexBasedMonolayerWithDeltaNotch();
+
+        return ExecutableSupport::EXIT_OK;
+    }
+    catch (const Exception &e)
+    {
+        ExecutableSupport::PrintError(e.GetMessage());
+        exit_code = ExecutableSupport::EXIT_ERROR;
+    }
+
+    // Optional - write the machine info to file.
+    ExecutableSupport::WriteMachineInfoFile("machine_info");
+
+    // End by finalizing PETSc, and returning a suitable exit code.
+    // 0 means 'no error'
+    ExecutableSupport::FinalizePetsc();
+    return exit_code;
+}
